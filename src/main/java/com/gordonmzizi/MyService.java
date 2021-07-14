1736024a416faa087f4ad32dac6553d14bd86b0e
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-07-15 00:30:19 +0200

    implementing setter based Dependency Injection

--- src/main/java/com/gordonmzizi/MyService.java
+++ src/main/java/com/gordonmzizi/MyService.java
@@ -5,12 +5,18 @@ public class MyService {
 
     //Implementing Constractor DI
 
-    public MyService(MyRepository repository) {
+    /*
+     public MyService(MyRepository repository) {
         this.repository = repository;
     }
+    */
 
     public void getBusinessLogic(){
         System.out.println("Doing Business Logic");
         repository.getQuery();
+    }
+
+    public void setRepository(MyRepository repository) {
+        this.repository = repository;
     }
 }
